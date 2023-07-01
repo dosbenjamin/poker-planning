de0ec0af53b1adf639bd5abc85038041e5d8afa9
Benjamin Dos Santos <hello@benjamindossantos.be>
2023-07-01 17:30:00 +0200

    feat: add form components

--- panda.config.ts
+++ panda.config.ts
@@ -1,16 +1,61 @@
-import { defineConfig, definePattern } from '@pandacss/dev';
+import { defineConfig, definePattern, defineTextStyles } from '@pandacss/dev';
 
 export default defineConfig({
   include: ['./src/**/*.{ts,tsx}'],
   jsxFramework: 'react',
   patterns: {
     extend: {
+      center: definePattern({
+        transform: () => ({
+          display: 'grid',
+          placeContent: 'center',
+        }),
+      }),
       container: definePattern({
         transform: () => ({
           bg: 'zinc.100',
           color: 'zinc.900',
+          display: 'flex',
+          flexDir: 'column',
+          gap: '4',
+          minWidth: 'md',
           p: '8',
-          rounded: 'md',
+          rounded: 'lg',
+        }),
+      }),
+      formControl: definePattern({
+        jsxElement: 'label',
+        transform: () => ({
+          display: 'flex',
+          flexDir: 'column',
+          gap: '2',
+        }),
+      }),
+      formInput: definePattern({
+        jsxElement: 'input',
+        transform: () => ({
+          _focus: {
+            '&:hover': {
+              borderColor: 'zinc.400',
+            },
+            borderColor: 'zinc.400',
+          },
+          _hover: {
+            borderColor: 'zinc.300',
+          },
+          borderColor: 'zinc.200',
+          borderWidth: 'thin',
+          display: 'block',
+          outline: 'none',
+          p: '4',
+          rounded: 'sm',
+          transition: 'border-color 250ms',
+        }),
+      }),
+      formLabel: definePattern({
+        jsxElement: 'span',
+        transform: () => ({
+          fontVariationSettings: '"wght" 550',
         }),
       }),
     },
@@ -18,6 +63,17 @@ export default defineConfig({
   preflight: true,
   theme: {
     extend: {
+      textStyles: defineTextStyles({
+        heading: {
+          page: {
+            value: {
+              fontSize: '4xl',
+              fontVariationSettings: '"wght" 800',
+              lineHeight: '1.2',
+            },
+          },
+        },
+      }),
       tokens: {
         fonts: {
           body: { value: 'var(--font-inter), var(--font-fallback)' },
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+import { FormControl, FormInput, FormLabel, styled } from 'styled-system/jsx';
 import { container } from 'styled-system/patterns';
 
 const HomePage = (): JSX.Element => {
   return (
     <main className={container()}>
-      <h1>Join a room</h1>
-      <form></form>
+      <styled.h1 textStyle="heading.page">Create a room</styled.h1>
+      <form>
+        <FormControl>
+          <FormLabel>Display name</FormLabel>
+          <FormInput placeholder="Insert a name for your new room" />
+        </FormControl>
+      </form>
     </main>
   );
 };
