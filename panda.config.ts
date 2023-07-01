ed018cbb420ad7c0c7b76ee0dfee3fbef31a1f8c
Benjamin Dos Santos <hello@benjamindossantos.be>
2023-07-01 11:52:07 +0200

    feat: config inter font with panda

--- panda.config.ts
+++ panda.config.ts
@@ -2,5 +2,15 @@ import { defineConfig } from '@pandacss/dev';
 
 export default defineConfig({
   include: ['./src/**/*.{ts,tsx}'],
+  jsxFramework: 'react',
   preflight: true,
+  theme: {
+    extend: {
+      tokens: {
+        fonts: {
+          body: { value: 'var(--font-inter), var(--font-fallback)' },
+        },
+      },
+    },
+  },
 });
